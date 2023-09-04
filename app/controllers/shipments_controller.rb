3cca385e78c76aaabe09ddc7ba462f1c111fbf17
Haya B <lacourt.christelle@gmail.com>
2023-09-04 16:51:38 +0100

    edit function added, need styling

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,23 +1,38 @@
 class ShipmentsController < ApplicationController
   def index
+    @shipments = @Shipment.all
   end
 
   def new
+    @shipment = Shipment.new
   end
 
   def create
   end
 
   def show
+    @shipment = Shipment.find(params[:id])
   end
 
   def edit
+    @shipment = Shipment.find(params[:id])
+    redirect_to shipments_path, notice: "Shipment successfully edited."
   end
 
   def update
+    @shipment = Shipment.find(params[:id])
+    if @shipment.update(shipment_params)
+      redirect_to shipment_path(@shipment), notice: "Shipment successfully edited."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
 
+  private
+  def shipment_params
+    params.require(:shipment).permit()
+  end
 end
