2bcc67a956211c4300f405dacef2bd202f177ad8
Hai Dinh NGUYEN <sandersonns@gmail.com>
2023-09-05 09:14:17 +0200

    codes index feature

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,5 +1,6 @@
 class ShipmentsController < ApplicationController
   def index
+    @shipments = Shipment.all
   end
 
   def new
@@ -18,6 +19,18 @@ class ShipmentsController < ApplicationController
   end
 
   def destroy
+    @shipment = Shipment.find(params[:id])
+
+    # Ensure that only the owner can update the planet
+    if current_user == @shipment.user
+      if @shipment.destroy!
+        redirect_to shipments_path, notice: "Planet was successfully deleted."
+      else
+        render :index
+      end
+    else
+      redirect_to shipments_path, alert: "You are not authorized to delete this planet."
+    end
   end
 
 end
