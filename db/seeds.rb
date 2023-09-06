cb87089d9a1e83e8263f22c6318a185826b3d98b
Kim van Ruiven <k.vanruiven@hotmail.com>
2023-09-06 12:11:37 +0200

    show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Coding this method on the class
+User.destroy_all
+Shipment.destroy_all
 
 hai = User.create(email: "hai@gmail.com", password: "hai@gmail.com", first_name: "hai dinh", last_name: "nguyen")
 
-#write seed
+# Create some products
+10.times do
+  Shipment.create(city: 'Rotterdam', vehicle_type: Faker::Vehicle.make_and_model, fuel_type: 'diesel', user: hai, distance_traveled: '10', fuel_consumption: '20', product_name: 'Nike', shipment_start: '13 september', shipment_end: '12 december', co2_emissions: '30')
+end
