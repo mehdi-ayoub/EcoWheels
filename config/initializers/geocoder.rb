33296b5c972b156a4e364ccbfdc3bb598916d65a
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-11-29 08:36:01 +0400

    solved the add shipment bug

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,9 @@
 Geocoder.configure(
   lookup: :mapbox,
-  api_key: "pk.eyJ1IjoibWVoZGlheW91YiIsImEiOiJjbG02anZpMm40cnBoM2pwdmMwbWFsem44In0.PcPnwBmrJJd_sGhV8G8JVQ",
+  api_key: ENV['MAPBOX_API_KEY'],
 
-  units: :km
+  units: :km,
+  use_https: true,
+
   # distances: :linear
 )
